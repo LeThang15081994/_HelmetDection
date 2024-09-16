f163285aaa393e0bbbec5609b4131585f581fee6
LeThang15081994 <lesythang1581994@gmail.com>
2024-09-16 13:23:46 +0700

    upgrade file

--- main.py
+++ main.py
@@ -21,11 +21,35 @@ def train_model():
                 epochs = epochs_size,
                 imgsz = img_size,
                 batch = batch_size,
-                device = '0')
-    # device ='0' to choose GPU
+                device = 'cuda')
+    # device ='cuda' to choose GPU
+
+def val_model(): 
+    
+    #config
+    model = YOLOv10('./yolov10/runs/detect/train/weights/best.pt')
+
+    batch_size = 16
+    path_yaml = "../safety-Helmet-Reflective-Jacket/data.yaml"
+
+    #Val
+    model.val(data=path_yaml,
+                batch = batch_size)
+
+def predic_model(): 
+    
+    #config
+    model = YOLOv10('./yolov10/runs/detect/train/weights/best.pt')
+
+    batch_size = 16
+    path_yaml = "../safety-Helmet-Reflective-Jacket/test/"
+
+    #Val
+    model.predict(source = "./safety-Helmet-Reflective-Jacket/test/images/", save = True, conf = 0.5)
+
    
 if __name__ == '__main__':
    #checks_torch()
    #train_model()
    #val_model()
-   predic_model()
+   #predic_model()
