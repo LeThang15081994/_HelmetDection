373fd62cb1edbc979ef4d8cbfabff1d3a6b586a1
LeThang15081994 <lesythang1581994@gmail.com>
2024-09-19 08:43:05 +0700

    upgrade file

--- main.py
+++ main.py
@@ -11,19 +11,19 @@ def train_model():
     
     #config
     model = YOLOv10('yolov10s.pt')
-
+    
     #model.info()
     epochs_size = 25
     img_size = 640
     batch_size = 16
     path_yaml = 'D:/WorkSpace/_thangle15894/_AIproject/_HelmetDetection/safety-Helmet-Reflective-Jacket/data_helmet.yaml'
-
+    devices = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     #train
     model.train(data=path_yaml,
                 epochs = epochs_size,
                 imgsz = img_size,
                 batch = batch_size,
-                device = 'cuda')
+                device = devices)
     # device ='cuda' to choose GPU
 
 def val_model(): 
@@ -34,12 +34,12 @@ def val_model():
     batch_size = 16
     path_yaml = "./safety-Helmet-Reflective-Jacket/data_helmet.yaml"
     img_size = 640
-
+    devices = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     #Val
     model.val(data=path_yaml,
                 batch = batch_size,
                 imgsz = img_size,
-                device = 'cuda',
+                device = devices,
                 split='test')
 
 def predic_model(): 
@@ -49,9 +49,9 @@ def predic_model():
     img_size = 640
     #predic
     results= model.predict(source = "./testImg.jpg", 
-                  imgsz = img_size,
-                  save = True, 
-                  conf = 0.40)
+                            imgsz = img_size,
+                            save = True, 
+                            conf = 0.40)
     img = results[0].plot()
     #img_bgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
     cv2.imshow("Predicted Image", img)
